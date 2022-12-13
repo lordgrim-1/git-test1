65e807c02c2ac2f003333937c318c3a44ced69ec
Lordgrim <Lordgrim@qq.com>
2022-12-13 20:28:31 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,10 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git1");
         System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
 
     }
 }
